87f180db247c398ac66ffc7459a5be8790c2e1c1
Manoj5621 <muruli5621@gmail.com>
2026-01-09 15:51:18 +0530

    Stranger things poster website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">STRANGER THINGS</div>
+
+    </nav>
+  )
+}
+
+export default Navbar
